0b95b4540205ca583120c5ecaef51ab613880621
iFanpS <89406191+iFanpSGTS@users.noreply.github.com>
2022-02-28 21:51:42 +0700

    added files

---
+++ itemsdat-decoder.py
@@ -0,0 +1,222 @@
+# full print all data
+# remaked by iFanpS; helped by KIPAS (alot of help)
+import struct
+SECRET = "PBG892FXX982ABC*"
+def xordec(id, nlen, pos, enc, data):
+    str = ''
+    if enc == True:
+        for i in range(nlen):
+            str += chr(data[pos])
+            pos += 1
+    else:
+        for i in range(nlen):
+            str += chr(data[pos] ^ ord(SECRET[(id + i) % len(SECRET)]))
+            pos += 1
+    return str
+
+def decFile(data):
+    memPos = 0
+    itemsdatVersion = struct.unpack('<H', data[memPos:memPos+2])[0]
+    memPos += 2
+    itemCount = struct.unpack('i', data[memPos:memPos+4])[0]
+    memPos += 4
+    for i in range(itemCount):
+        itemID = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 4
+        
+        editableType = data[memPos]
+        memPos += 1
+        itemCategory = data[memPos]
+        memPos += 1
+        actionType = data[memPos]
+        memPos += 1
+        hitSoundType = data[memPos]
+        memPos += 1
+        
+        ##name parse
+        strLen = data[memPos] + data[memPos + 1] * 256
+        memPos += 2
+        name = xordec(itemID,strLen,memPos, False, data)
+        memPos += strLen
+        ##name parse end
+        
+        ##texture parse
+        strLen = data[memPos] + data[memPos + 1] * 256
+        memPos += 2
+        texture = xordec(itemID,strLen,memPos, True, data)
+        memPos += strLen
+        ##texture parse
+        
+        #texturehash parse
+        textureHash = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 4
+        #end
+        
+        #itemKind
+        itemKind = data[memPos]
+        memPos += 1
+        #end
+        
+        #val1 parse
+        val1 = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 4
+        #end
+        
+        #textureX & textureY
+        textureX = data[memPos]
+        textureY = data[memPos]
+        memPos += 2
+        #end
+        
+        #spreadType
+        spreadType = data[memPos]
+        memPos += 1
+        #end
+        
+        #stripeywallpaper
+        isStripeyWallpaper = data[memPos]
+        memPos += 1
+        #end
+        
+        #Collision
+        collisionType = data[memPos]
+        memPos += 1
+        #end
+        
+        #breakHits
+        breakHits = data[memPos]
+        memPos += 1
+        #end
+        
+        #dropChance
+        dropChance = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 4
+        #end
+        
+        #clothingType 
+        clothingType = data[memPos]
+        memPos += 1
+        #end
+        
+        #rarity
+        rarity = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 2
+        #end
+        
+        #maxAmount
+        maxAmount = data[memPos]
+        memPos += 1
+        #end
+        
+        ##extrFile parse
+        strLen = data[memPos] + data[memPos + 1] * 256
+        memPos += 2
+        extraFile = xordec(itemID,strLen,memPos, True, data)
+        memPos += strLen
+        ##extraFile parse
+        
+        #extraFilehash
+        extraFilehash = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 4
+        #end
+        
+        #audioVolume
+        audioVolume = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 4
+        #end
+        
+        ##pet option
+        strLen = data[memPos] + data[memPos + 1] * 256
+        memPos += 2
+        petName = xordec(itemID,strLen,memPos, True, data)
+        memPos += strLen
+        
+        strLen = data[memPos] + data[memPos + 1] * 256
+        memPos += 2
+        petPrefix = xordec(itemID,strLen,memPos, True, data)
+        memPos += strLen
+        
+        strLen = data[memPos] + data[memPos + 1] * 256
+        memPos += 2
+        petSuffix = xordec(itemID,strLen,memPos, True, data)
+        memPos += strLen
+        
+        strLen = data[memPos] + data[memPos + 1] * 256
+        memPos += 2
+        petAbillity = xordec(itemID,strLen,memPos, True, data)
+        memPos += strLen
+        ##pet option
+        
+        #seed(base,overlay)
+        seedBase = data[memPos]
+        seedOverlay = data[memPos]
+        memPos += 2
+        #end
+        
+        #tree(base,leaves)
+        treeLeaves = data[memPos]
+        treeBase = data[memPos]
+        memPos += 2
+        #end
+        
+        #seed(color,overlaycolor)
+        seedColor = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 4
+        seedOverlayColor = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 4
+        #end
+        
+        memPos += 4 #deleted ingridients
+        
+        #growTime
+        growTime = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 4
+        #end
+        
+        #val2 & isRayman
+        val2 = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 2
+        isRayman = struct.unpack('i', data[memPos:memPos+4])[0]
+        memPos += 2
+        #end
+        
+        ##item extra data
+        strLen = data[memPos] + data[memPos + 1] * 256
+        memPos += 2
+        extraOptions = xordec(itemID,strLen,memPos, True, data)
+        memPos += strLen
+        
+        strLen = data[memPos] + data[memPos + 1] * 256
+        memPos += 2
+        texture2 = xordec(itemID,strLen,memPos, True, data)
+        memPos += strLen
+        
+        strLen = data[memPos] + data[memPos + 1] * 256
+        memPos += 2
+        extraOptions2 = xordec(itemID,strLen,memPos, True, data)
+        memPos += strLen
+        
+        ##item extra data
+        
+        memPos += 80; #unknown data
+        
+        if (itemsdatVersion >= 11):
+            strLen = data[memPos] + data[memPos + 1] * 256
+            memPos += 2
+            punchOptions = xordec(itemID,strLen,memPos, True, data)
+            memPos += strLen
+            
+        if (itemsdatVersion >= 12):
+            memPos += 13
+        
+        if (itemsdatVersion >= 13):
+            memPos += 4
+            
+        memPos += 4 #skip some data
+        print(seedBase)
+        if i != itemID:
+            print('Unordered item ' + str(itemID) + '/' + str(itemCount))
+    
+
+a = input('items.dat loc/name: ')
+decFile(open(a, 'rb').read())
